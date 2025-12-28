fb532a885253d22088b5e93141a40150c5a77c2c
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-12-28 01:11:00 +0000

    add header to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
 
   return (
     <div className={appCSS.container}>
+      <div className={appCSS.header}>
+        <h2> Memory Game </h2>
+        <div className={appCSS.scoreGroup}>
+            <div>Current Score</div>
+            <div>Best Score</div>
+        </div>
+      </div>
     </div>
   )
 }
