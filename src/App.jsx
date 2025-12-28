78b28aeef32bb9eeb7b8ad5c4f037b8ca6610eac
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-12-28 00:36:08 +0000

    import css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import appCSS from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className={appCSS.container}>
+    </div>
   )
 }
 
