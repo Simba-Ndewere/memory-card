fa74cffa5259d9f988de2a136fbba8227dd90cf0
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-12-28 23:53:29 +0000

    add emojis grid container

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ function App() {
   return (
     <div className={appCSS.container}>
       <div className={appCSS.header}>
-        <h2> Memory Game </h2>
+        <h2> Emoji Rush </h2>
         <div className={appCSS.scoreGroup}>
             <div>Current Score</div>
             <div>Best Score</div>
         </div>
+      </div>
+      <div className={appCSS.emojiContainer}>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
+          <div className={appCSS.item}>Item 1</div>
       </div>
     </div>
   )
